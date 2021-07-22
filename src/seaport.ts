ca40286c86859544ca1063e373d1fea74a64ec85
we00013310 <we00013310@gmail.com>
2021-07-22 13:00:22 +0800

    v1.0.28

--- src/seaport.ts
+++ src/seaport.ts
@@ -3677,18 +3677,18 @@ export class OpenSeaPort {
         );
       }
 
-      // Check WETH balance
-      if (balance.toNumber() < minimumAmount.toNumber()) {
-        if (
-          tokenAddress ==
-          // @ts-ignore
-          WyvernSchemas.tokens[this._networkName].canonicalWrappedEther.address
-        ) {
-          throw new Error("Insufficient balance. You may need to wrap Ether.");
-        } else {
-          throw new Error("Insufficient balance.");
-        }
-      }
+      // // Check WETH balance
+      // if (balance.toNumber() < minimumAmount.toNumber()) {
+      //   if (
+      //     tokenAddress ==
+      //     // @ts-ignore
+      //     WyvernSchemas.tokens[this._networkName].canonicalWrappedEther.address
+      //   ) {
+      //     throw new Error("Insufficient balance. You may need to wrap Ether.");
+      //   } else {
+      //     throw new Error("Insufficient balance.");
+      //   }
+      // }
 
       // Check token approval
       // This can be done at a higher level to show UI
